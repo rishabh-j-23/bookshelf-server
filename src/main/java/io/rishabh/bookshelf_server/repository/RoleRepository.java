6165fb1a469bfb25723603fcc37de2ea4ec06628
Rishabh Jain <rjain23112002@gmail.com>
2024-08-25 11:17:59 +0530

    refactor

--- src/main/java/io/rishabh/bookshelf_server/repository/RoleRepository.java
+++ src/main/java/io/rishabh/bookshelf_server/repository/RoleRepository.java
@@ -1,9 +1,12 @@
-package io.rishabh.bookshelf_server.security.auth.roles;
+package io.rishabh.bookshelf_server.repository;
 
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import io.rishabh.bookshelf_server.security.auth.roles.Role;
+import io.rishabh.bookshelf_server.security.auth.roles.UserRole;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
